4533d89e4a3d085f39b13b504714bda6ab905a4b
Alex <wasplexa54@gmail.com>
2022-04-16 10:33:16 +0300

    FinalTask

---
+++ src/test/java/YandexMarket/framework/brawser/elemets/Text.java
@@ -0,0 +1,10 @@
+package YandexMarket.framework.brawser.elemets;
+
+import org.openqa.selenium.By;
+
+public class Text extends AbstractElement{
+    public Text(By locator, String name) {
+        super(locator, name);
+    }
+
+}
